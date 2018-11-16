7974b9ab4a81ff4d5bdd636384e33102dedbd76d
Derek Sayler <sayler.derek@gmail.com>
2018-11-16 14:25:46 -0800

    Added leave on out cross validation. Needs more testing

--- main.py
+++ main.py
@@ -1,86 +1,175 @@
 import os
-import random
 import numpy as np
 
 
 N = 200 #Number of data points
 M = 10 #Number of features for each data point
 #M = 100 #Number of features for each data point LARGE data set only
-file = 'data/CS170_SMALLtestdata__96.txt' #path to data set txt
+
+#path to data set txt
+file = 'data/CS170_SMALLtestdata__96.txt'
+#file = 'data/CS170_SMALLtestdata__110.txt'
 #file = 'data/CS170_LARGEtestdata__58.txt'
+#file = 'data/CS170_SMALLtestdata__SAMPLE.txt'
 
 def readdataset():
     # Read in data
-    data = []
+    datapy = []
     fileDir = os.path.dirname(os.path.realpath('__file__'))
     filename = os.path.join(fileDir, file)
-    with open(filename, "r") as infile:
+
+    with open(filename, "r") as infile: #read in data from file
         for line in infile:
             row_list = []
             data_list = line.split()
             for i in range(M+1):
                 row_list.append(float(data_list[i]))
-            data.append(row_list)
+            datapy.append(row_list)
 
-    for i in range(N):
-        print(i+1, data[i])
+    datanp = np.array(datapy) #turn in numpy array for easier functionality
 
-    return data
+    return datanp
 
-def crossvalidation(data, currentfeatures, j):
-    return random.randint(1, 101)
+def createtestsets(data, currentfeatures, entry):
+    # create training data set and leave on out test point based on current features
+    # data is dataset currently being worked on
+    # currentfeatures to be tested
+    # entry is the row that the testdata point will be
+
+    testdatatmp = []
+    testdatatmp.append(data[entry][0])
+    for j in currentfeatures:
+        testdatatmp.append(data[entry][j])
+    testdata = np.array(testdatatmp)
+
+    #print(testdata)
+
+    traindatatmp = []
+    for k in range(len(data)):
+        traininit = []
+        traininit.append(data[k][0])
+        for l in currentfeatures:
+            traininit.append(data[k][l])
+        traindatatmp.append(traininit)
+    traindata = np.array(traindatatmp)
+    traindata = np.delete(traindata, entry, 0)
+
+    #print(traindata)
+
+    return traindata, testdata
+
+def nearestneighbor(traindata, testdata):
+    # nearest neighbors
+
+    distances = []
+    for i in range(len(traindata)):
+        distance = np.sqrt(np.sum(np.square(testdata[1:] - traindata[i, 1:])))#find euclidean distance
+        distances.append([distance, i])
+    distances = sorted(distances)
+    return distances[0][1]#return index of point with shortest distance
+
+def leave_one_out_crossvalidation(data, currentfeatures, j, choice):
+    # Leave one out cross validation
+    # testdata is the data point thats left out
+    # traindata is the data set with testdata removed
+
+    features = []
+    for i in currentfeatures:
+        features.append(i)
+    if choice == 1:
+        features.append(j)
+    elif choice == 2:
+        features.remove(j)
+
+    closest = []
+    numcorrect = 0
+    for i in range(len(data)):
+        traindata, testdata = createtestsets(data, features, i)
+        closest.append(nearestneighbor(traindata, testdata))
+    for i in range(len(closest)):
+        if testdata[0] == traindata[closest[i]][0]:
+            numcorrect = numcorrect + 1
+    accuracy = float(numcorrect) / len(data) * 100
+    return accuracy
+
+def backwardsselection(data, choice):
+    # Backward selection for features
+
+    print()
+    print("Backward selection")
+    currentfeatures = []
+    for i in range(1, M + 1):
+        currentfeatures.append(i)
+    for i in range(1, M + 1):
+        print("On the ", i, "th level of the search tree", sep='')
+        feature = []
+        bestaccuracy = 0
+        for j in range(1, M + 1):
+            if j in currentfeatures:
+                accuracy = leave_one_out_crossvalidation(data, currentfeatures, j, choice)
+                print("Considering removing feature", j, "with accuracy", accuracy)
+                if accuracy > bestaccuracy:
+                    bestaccuracy = accuracy
+                    feature = j
+        currentfeatures.remove(feature)
+        print('Level ', i, ': removed feature ', feature, ' from current set with accuracy ', bestaccuracy, '%', sep='')
+        print("Feature list: ", currentfeatures)
+        print()
+
+def forwardselection(data, choice):
+    # Forward selection for features
+
+    print()
+    print("Forward selection")
+    currentfeatures = []
+    for i in range(1, M + 1):
+        print("On the ", i, "th level of the search tree", sep='')
+        feature = []
+        bestaccuracy = 0
+        for j in range(1, M + 1):
+            if j not in currentfeatures:
+                accuracy = leave_one_out_crossvalidation(data, currentfeatures, j, choice)
+                print("Considering adding feature", j, "with accuracy", accuracy)
+                if accuracy > bestaccuracy:
+                    bestaccuracy = accuracy
+                    feature = j
+        currentfeatures.append(feature)
+        print('Level ', i, ': added feature ', feature, ' to current set with accuracy ', bestaccuracy, '%', sep='')
+        print("Feature list: ", currentfeatures)
+        print()
+
+def holdit(data, feature):
+
+    closest = []
+    numcorrect = 0
+    for i in range(len(data)):
+        traindata, testdata = createtestsets(data, [], i)
+        closest.append(nearestneighbor(traindata, testdata))
+    for i in range(len(closest)):
+        if testdata[0] == traindata[closest[i]][0]:
+            numcorrect = numcorrect + 1
+    accuracy = (float(numcorrect) / len(data)) * 100
+    print(traindata[89])
+    print(testdata[1:])
+    print(accuracy)
 
 def main():
 
     data = readdataset()
+
+    print("Welcome to Derek Sayler's nearest neighbor classifier with feature selection program")
+    print("1. Forward Selection")
+    print("2. Backwards Selection")
+    print("3. Derek's Custom Algorithm")
     print()
-    print()
-    print("Forward selection")
-
-    # Forward selection
-    currentfeatures = []
-    for i in range(1,M+1):
-        print("On the ", i, "th level of the search tree", sep='')
-        addfeature = []
-        bestaccuracy = 0
-        for j in range(1,M+1):
-            if j not in currentfeatures:
-                print("Considering adding", j, "feature")
-                accuracy = crossvalidation(data, currentfeatures, j)
-                if accuracy > bestaccuracy:
-                    bestaccuracy = accuracy
-                    addfeature = j
-        currentfeatures.append(addfeature)
-        print('Level', i, 'added feature', addfeature, 'to current set')
-    print()
-    print(currentfeatures)
-
-    print()
-    print("Backward selection")
-
-    # Backward selection
-    currentfeatures = []
-    for i in range(1, M+1):
-        currentfeatures.append(i)
-
-    for i in range(1, M + 1):
-        print("On the ", i, "th level of the search tree", sep='')
-        removefeature = []
-        bestaccuracy = 0
-        for j in range(1, M + 1):
-            if j in currentfeatures:
-                print("Considering removing", j, "feature")
-                accuracy = crossvalidation(data, currentfeatures, j)
-                if accuracy > bestaccuracy:
-                    bestaccuracy = accuracy
-                    removefeature = j
-        currentfeatures.remove(removefeature)
-        print('Level', i, 'removed feature', removefeature, 'to current set')
-    print()
-    print(currentfeatures)
-
-
-
-
+    print("Enter which algorithm you want")
+    choice = int(input())
+    if choice is 1:
+        forwardselection(data, choice)
+    elif choice is 2:
+        backwardsselection(data, choice)
+    else:
+        print('ERROR: Invalid option')
+    return
 
 main()
